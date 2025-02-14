d119675df945d5fbf80f971e9cba6914611a9922
Drew-Andersen <andrewtandersen@gmail.com>
2025-02-14 17:10:58 -0500

    Added some bootstrap

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -9,15 +9,18 @@ def index(req):
 
     if req.method == 'POST':
         city = req.POST.get('city')
+        state = req.POST.get('state')
+        country = "US"
+
         if city:
-            geo_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}'
+            geo_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={api_key}'
             response = requests.get(geo_url)
             if response.status_code == 200 and response.json():
                 geo_data = response.json()[0]
-                state = geo_data.get('state', '')
-                country = geo_data.get('country', '')
+                lat, lon = geo_data['lat'], geo_data['lon']
+                full_state = geo_data.get('state', state)
 
-                weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}&units=metric'
+                weather_url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
                 weather_res = requests.get(weather_url)
 
                 if weather_res.status_code == 200:
@@ -26,6 +29,8 @@ def index(req):
                     celc_temp = weather_data['main']['temp']
                     fahr_temp = (celc_temp * 9/5) + 32
                     weather_data['main']['temp_f'] = round(fahr_temp, 2)
+
+                    weather_data['state'] = full_state
                 else:
                     error_message = 'Weather data not found. Please try again.'
             else:
